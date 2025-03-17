9a9c62143e8117acfa1b8737347cbf1b361d9937
Nayetdet <jpmoreiracampos21@gmail.com>
2025-03-17 02:36:49 -0300

    feat: adicionado rascunho inicial das entidades do projeto

---
+++ backend/src/main/java/br/com/edu/ifce/maracanau/carekobooks/enums/BookProgressStatus.java
@@ -0,0 +1,9 @@
+package br.com.edu.ifce.maracanau.carekobooks.enums;
+
+public enum BookProgressStatus {
+
+    READING,
+    PLANS_TO_READ,
+    FAVORITED
+
+}
---
+++ backend/src/main/java/br/com/edu/ifce/maracanau/carekobooks/model/Thread.java
@@ -0,0 +1,32 @@
+package br.com.edu.ifce.maracanau.carekobooks.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table
+public class Thread extends AbstractEntity {
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(length = 1000, nullable = false)
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    @OneToMany(mappedBy = "thread")
+    private List<ThreadReply> replies;
+
+}
