9348c59b1daf6459200bf8bbaf420146151328c0
Nayetdet <jpmoreiracampos21@gmail.com>
2025-03-20 17:05:30 -0300

    feat: iniciado conserto do forum

--- backend/src/main/java/br/com/edu/ifce/maracanau/carekobooks/module/forum/application/dto/ForumDTO.java
+++ backend/src/main/java/br/com/edu/ifce/maracanau/carekobooks/module/forum/application/dto/ForumDTO.java
@@ -1,5 +1,7 @@
 package br.com.edu.ifce.maracanau.carekobooks.module.forum.application.dto;
 
+import br.com.edu.ifce.maracanau.carekobooks.module.book.application.dto.BookDTO;
+import br.com.edu.ifce.maracanau.carekobooks.module.user.application.dto.UserDTO;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,5 +14,7 @@ public class ForumDTO {
     private Long id;
     private String title;
     private String description;
+    private UserDTO user;
+    private BookDTO book;
 
 }
