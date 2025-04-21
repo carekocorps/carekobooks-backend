506125d077b22e46c567a2cf215eb8378d72e687
Nayetdet <jpmoreiracampos21@gmail.com>
2025-04-21 19:32:17 -0300

    fix: corrigido endpoint do websocket

--- src/main/java/br/com/edu/ifce/maracanau/carekobooks/common/config/WebSocketConfig.java
+++ src/main/java/br/com/edu/ifce/maracanau/carekobooks/common/config/WebSocketConfig.java
@@ -14,14 +14,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry
                 .addEndpoint("/ws")
-                .setAllowedOriginPatterns("*")
-                .withSockJS();
+                .setAllowedOriginPatterns("*");
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/topic");
+        registry
+                .setApplicationDestinationPrefixes("/app")
+                .enableSimpleBroker("/topic");
     }
 
 }
