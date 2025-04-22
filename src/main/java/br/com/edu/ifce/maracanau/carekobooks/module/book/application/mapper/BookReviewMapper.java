ef115d5af17ce7aec8fc95c409cf6f9cbe4a74db
Nayetdet <jpmoreiracampos21@gmail.com>
2025-04-22 13:37:19 -0300

    ref: refatorados os mappers

--- src/main/java/br/com/edu/ifce/maracanau/carekobooks/module/book/application/mapper/BookReviewMapper.java
+++ src/main/java/br/com/edu/ifce/maracanau/carekobooks/module/book/application/mapper/BookReviewMapper.java
@@ -3,22 +3,26 @@ package br.com.edu.ifce.maracanau.carekobooks.module.book.application.mapper;
 import br.com.edu.ifce.maracanau.carekobooks.module.book.application.representation.response.BookReviewResponse;
 import br.com.edu.ifce.maracanau.carekobooks.module.book.application.representation.request.BookReviewRequest;
 import br.com.edu.ifce.maracanau.carekobooks.module.book.infrastructure.model.BookReview;
-import br.com.edu.ifce.maracanau.carekobooks.module.user.application.mapper.UserContextMapper;
 import br.com.edu.ifce.maracanau.carekobooks.module.user.application.mapper.UserMapper;
 import br.com.edu.ifce.maracanau.carekobooks.common.layer.application.mapper.BaseUpdateMapper;
+import br.com.edu.ifce.maracanau.carekobooks.module.user.application.security.provider.UserContextProvider;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.springframework.beans.factory.annotation.Autowired;
 
-@Mapper(componentModel = "spring", config = BaseUpdateMapper.class, uses = {UserMapper.class, BookMapper.class})
-public abstract class BookReviewMapper implements BaseUpdateMapper<BookReview, BookReviewRequest> {
+@Mapper(
+        componentModel = "spring",
+        config = BaseUpdateMapper.class,
+        imports = UserContextProvider.class,
+        uses = {
+                UserMapper.class,
+                BookMapper.class
+        }
+)
+public interface BookReviewMapper extends BaseUpdateMapper<BookReview, BookReviewRequest> {
 
-    @Autowired
-    protected UserContextMapper userContextMapper;
-
-    @Mapping(target = "user", expression = "java(userContextMapper.toModel())")
+    @Mapping(target = "user", expression = "java(UserContextProvider.getUser())")
     @Mapping(target = "book", expression = "java(bookMapper.toModel(request.getBookId()))")
-    public abstract BookReview toModel(BookReviewRequest request);
-    public abstract BookReviewResponse toResponse(BookReview bookReview);
+    BookReview toModel(BookReviewRequest request);
+    BookReviewResponse toResponse(BookReview bookReview);
 
 }
