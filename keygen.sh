f26f1afeb92f3bce49ef30c27709469366778eff
Nayetdet <jpmoreiracampos21@gmail.com>
2025-05-20 07:53:56 -0300

    chore: melhorada imagem do dockerfile

--- keygen.sh
+++ keygen.sh
@@ -5,22 +5,21 @@ if ! command -v openssl &> /dev/null; then
     exit 1
 fi
 
-readonly OUTPUT_DIR=".secrets/keys"
-readonly PRIVATE_KEY="$OUTPUT_DIR/app.key"
-readonly PUBLIC_KEY="$OUTPUT_DIR/app.pub"
+readonly OUTPUT_DIR="${1:-.secrets/keys}"
+readonly PRIVATE_KEY_DIR="$OUTPUT_DIR/app.key"
+readonly PUBLIC_KEY_DIR="$OUTPUT_DIR/app.pub"
 
-mkdir -p "$OUTPUT_DIR"
-openssl genpkey -algorithm RSA -out "$PRIVATE_KEY" -pkeyopt rsa_keygen_bits:2048 &> /dev/null
-if [ ! -f "$PRIVATE_KEY" ]; then
+openssl genpkey -algorithm RSA -out "$PRIVATE_KEY_DIR" -pkeyopt rsa_keygen_bits:2048 &> /dev/null
+if [ ! -f "$PRIVATE_KEY_DIR" ]; then
     echo "Error generating the private key" >&2
     exit 1
 fi
 
-openssl rsa -pubout -in "$PRIVATE_KEY" -out "$PUBLIC_KEY" &> /dev/null
-if [ ! -f "$PUBLIC_KEY" ]; then
+openssl rsa -pubout -in "$PRIVATE_KEY_DIR" -out "$PUBLIC_KEY_DIR" &> /dev/null
+if [ ! -f "$PUBLIC_KEY_DIR" ]; then
     echo "Error generating the public key" >&2
     exit 1
 fi
 
-chmod 644 "$PRIVATE_KEY" "$PUBLIC_KEY" &> /dev/null
+chmod 644 "$PRIVATE_KEY_DIR" "$PUBLIC_KEY_DIR" &> /dev/null
 echo "RSA Keys generated successfully"
