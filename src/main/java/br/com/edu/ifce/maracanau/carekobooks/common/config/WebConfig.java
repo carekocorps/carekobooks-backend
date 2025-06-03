a8795404cd94529d185ed45a9c1ed15d4d2d8e99
Isaac Duarte <isaacunhaid@gmail.com>
2025-06-03 17:04:58 -0300

    fix: refatorando CORS

--- src/main/java/br/com/edu/ifce/maracanau/carekobooks/common/config/WebConfig.java
+++ src/main/java/br/com/edu/ifce/maracanau/carekobooks/common/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("https://staging.carekobooks.space",
+                .allowedOriginPatterns("https://api.carekobooks.space",
                         "http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
