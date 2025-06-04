2e6580690a3b1e6aa85fc4aa9fe802bc7f3a370d
Nayetdet <jpmoreiracampos21@gmail.com>
2025-06-03 21:15:07 -0300

    ref: ajustado o armazenamento das origens permitadas do CORS

--- src/main/java/br/com/edu/ifce/maracanau/carekobooks/common/config/WebConfig.java
+++ src/main/java/br/com/edu/ifce/maracanau/carekobooks/common/config/WebConfig.java
@@ -1,17 +1,22 @@
 package br.com.edu.ifce.maracanau.carekobooks.common.config;
 
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@ConfigurationProperties(prefix = "security.web.cors")
 public class WebConfig implements WebMvcConfigurer {
+
+    @Setter
+    private String[] allowedOrigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("https://api.carekobooks.space",
-                        "http://localhost:3000")
+                .allowedOriginPatterns(allowedOrigins)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
